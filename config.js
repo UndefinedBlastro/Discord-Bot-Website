896f4caaa24d7506735462e0e5e5cf31857ec135
UndefinedBlastro <149256010+UndefinedBlastro@users.noreply.github.com>
2023-11-24 14:37:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-import {BsFacebook, BsYoutube } from "react-icons/bs";
-import {IoRocketSharp} from "react-icons/io5";
-import {FaDiscord} from "react-icons/fa";
-import {MdOutlineAddModerator} from "react-icons/md";
-import {GiTrophyCup} from "react-icons/gi";
-import {BiSolidMessageSquareEdit} from "react-icons/bi";
-import {SiWelcometothejungle} from "react-icons/si";
-import {SiFunimation} from "react-icons/si";
+import { BsFacebook, BsYoutube } from "react-icons/bs";
+import { CgCrown } from "react-icons/cg";
+import { FaDiscord } from "react-icons/fa";
+import { MdAddModerator } from "react-icons/md";
+import { SiProbot } from "react-icons/si";
+import { TbMessage2Code } from "react-icons/tb";
 const config = {
   name: "Ayra",
   //BotName
@@ -17,37 +15,37 @@ const config = {
       name: "Moderation",
       description:
         "Moderation is the process of eliminating or lessening extremes.",
-      logo: <MdOutlineAddModerator className="h-10 w-10" />,
+      logo: <MdAddModerator className="h-10 w-10" />,
     },
     {
       name: "99% Uptime",
       description:
         "Uptime is a measure of system reliability, expressed as the percentage of time a machine, typically a computer.",
-      logo: <IoRocketSharp className="h-10 w-10" />,
+      logo: <SiProbot className="h-10 w-10" />,
     },
     {
       name: "Automation",
       description:
-        "Want to automate your posts? We got you",
-      logo: <BiSolidMessageSquareEdit className="h-10 w-10" />,
+        "Automation describes a wide range of technologies that reduce  human intervention in processes.",
+      logo: <TbMessage2Code className="h-10 w-10" />,
     },
     {
       name: "Welcomer",
       description:
-        "Welcome new members in your style",
-      logo: <SiWelcometothejungle className="h-10 w-10" />,
+        "Welcomer is the person who greet the new person in an unique and great way",
+      logo: <CgCrown className="h-10 w-10" />,
     },
     {
-       name: "Leveling",
+      name: "Leveling",
       description:
-        "Spice up your server by our overhauled leveling system",
-      logo: <GiTrophyCup className="h-10 w-10" />,
+        "Welcomer is the person who greet the new person in an unique and great way",
+      logo: <CgCrown className="h-10 w-10" />,
     },
     {
-      name: "Fun, Actions and More..",
+      name: "Fun, Actions and more",
       description:
-        "Replace many bots with ayra and enjoy",
-      logo: <SiFunimation  className="h-10 w-10" />,
+        "Welcomer is the person who greet the new person in an unique and great way",
+      logo: <CgCrown className="h-10 w-10" />,
     },
     // You can added more features if need add
   ],
